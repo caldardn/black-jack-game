3922e7132f775971a5f78af4d62cee4a3de357b3
caldardn <caldardn@gmail.com>
2023-07-19 22:07:32 -0400

    Adding the final code

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,48 @@
-
-    
-
-let firstCard = 10
-let secondCard = 4
-let card = [firstCard, secondCard]
-let sum = firstCard + secondCard 
+let player = {
+    name: "David",
+    chips: 145
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard(){
+   let randomNumber = Math.floor(Math.random() * 13) + 1
+   
+   if (randomNumber > 10){
+    return 10
+   }else if(randomNumber === 1) {
+    return 11
+   } else  {
+    return randomNumber
+   }
+}
 
 function start(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard 
     renderGame()
 }
 
-
-
 function renderGame(){
-cardsEl.textContent = "Cards: " + card[0] + ", " + card[1]
+cardsEl.textContent = "Cards: "
+
+for(let i = 0; i < cards.length; i++){
+    cardsEl.textContent += cards[i] + " "
+}
+
 sumEl.textContent = "Sum: " + sum
     if (sum <= 20){
         message = "Would you like to draw a card?"
@@ -33,8 +56,15 @@ sumEl.textContent = "Sum: " + sum
 messageEl.textContent = message
 }
 
+
 function newCard(){
-    let card = 5
+    if(isAlive === true && hasBlackJack === false){
+        
+    let card = getRandomCard()
     sum += card
+    cards.push(card)
+    
    renderGame() 
+    }
 }
+
